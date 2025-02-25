e4f2ea91439a4ecab2ab9d5f695770b0a1547c76
nadine.loepfe <nadine.loepfe@swirldslabs.com>
2025-02-25 15:28:03 +0100

    auditmanagement upload to ipfs
    Signed-off-by: nadine.loepfe <nadine.loepfe@swirldslabs.com>

--- packages/fe-tw/src/app/admin/auditmanagement/page.tsx
+++ packages/fe-tw/src/app/admin/auditmanagement/page.tsx
@@ -1,9 +1,9 @@
-import { AuditManagementView } from "@/components/Admin/AuditManagementView";
+import { AuditManagementForm } from "@/components/Admin/AuditManagement";
 
 export default function AuditManagementPage() {
   return (
     <div className="p-4">
-      <AuditManagementView />
+      <AuditManagementForm />
     </div>
   );
 }
