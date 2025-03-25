bbadd4ea185055e02d087ba07de4efac08beea12
mykyta_yefruiev <efruev@gmail.com>
2025-03-25 12:50:37 +0100

    Added shad/cn and updated navigation bar
    Signed-off-by: mykyta_yefruiev <efruev@gmail.com>

--- packages/fe-tw/src/components/Navbar/Navbar.tsx
+++ packages/fe-tw/src/components/Navbar/Navbar.tsx
@@ -1,100 +1,99 @@
 "use client";
 
-import { NavbarUserActionsMenu } from "@/components/Navbar/NavbarUserActionsMenu";
-import { WalletConnectModalRW } from "@/components/Wallets/WalletConnectModalRW";
 import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
+import React, { ReactNode } from "react";
 import {
-  NavigationMenu, NavigationMenuContent,
-  NavigationMenuItem, NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle
+   NavigationMenu,
+   NavigationMenuContent,
+   NavigationMenuItem,
+   NavigationMenuLink,
+   NavigationMenuList,
+   NavigationMenuTrigger,
+   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import {cn} from "@/lib/utils";
-import {Button} from "@/components/ui/button";
-import {Wallet} from "lucide-react";
-
-const SITE_LINKS = [
-  { title: "FAQ", url: "/faq" },
-  { title: "Admin", url: "/admin" },
-];
+import { cn } from "@/lib/utils";
+import { Building, Earth, Radar, Slice } from "lucide-react";
+import { WalletConnectModalRW } from "../Wallets/WalletConnectModalRW";
 
 export function Navbar() {
-  return (
-      <NavigationMenu className="justify-end p-4 min-w-[100vw]">
-        <NavigationMenuList
-            // className="flex justify-end p-4 min-w-[100vw]"
-        >
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Explorer</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul
-                  // className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]"
-              >
-                <ListItem
-                    title="Featured"
-                    href="/explorer"
-                >
-                  Dive into the world of our picks for You to explore
-                </ListItem>
-                <ListItem
-                    title="Buildings"
-                    href="/building"
-                >
-                  Open the door to the world of tokenized buildings
-                </ListItem>
-                <ListItem
-                    title="Slices"
-                    href="/slices"
-                >
-                  Optimize your portfolio with our building slices
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
+   return (
+      <div className="min-w-[100vw] flex justify-end p-4 border-b border-base-200 items-center sticky top-0 z-50 bg-white">
+         <Link href="/landing" className="mr-auto flex gap-1 items-center">
+            <Earth />
+            <p className="italic font-bold text-xl text-violet-700">RWA</p>
+         </Link>
+         <NavigationMenu>
+            <NavigationMenuList className="gap-3">
+               <NavigationMenuItem>
+                  <NavigationMenuTrigger>Explorer</NavigationMenuTrigger>
+                  <NavigationMenuContent asChild data-state="open">
+                     <ul className="grid w-[300px] gap-2 p-1 md:w-[300px] md:grid-cols-1 lg:w-[300px]">
+                        <ListItem icon={<Radar />} title="Featured" href="/explorer">
+                           Dive into the world of our picks for You to explore
+                        </ListItem>
+                        <ListItem icon={<Building />} title="Buildings" href="/building">
+                           Open the door to the world of tokenized buildings
+                        </ListItem>
+                        <ListItem icon={<Slice />} title="Slices" href="/slices">
+                           Optimize your portfolio with our building slices
+                        </ListItem>
+                     </ul>
+                  </NavigationMenuContent>
+               </NavigationMenuItem>
 
-          </NavigationMenuItem>
+               <NavigationMenuItem>
+                  <Link href="/faq" legacyBehavior>
+                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        FAQ
+                     </NavigationMenuLink>
+                  </Link>
+               </NavigationMenuItem>
 
-          <NavigationMenuItem>
-            <Link href='/faq' legacyBehavior>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>FAQ</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <Link href='/admin' legacyBehavior>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Admin</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Button><Wallet />Connect</Button>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>);
+               <NavigationMenuItem>
+                  <Link href="/admin" legacyBehavior>
+                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Admin
+                     </NavigationMenuLink>
+                  </Link>
+               </NavigationMenuItem>
+               <NavigationMenuItem>
+                  <WalletConnectModalRW />
+               </NavigationMenuItem>
+            </NavigationMenuList>
+         </NavigationMenu>
+      </div>
+   );
 }
 
 const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
+   React.ElementRef<"a">,
+   React.ComponentPropsWithoutRef<"a"> & { icon: ReactNode }
+>(({ className, icon, title, children, ...props }, ref) => {
+   return (
       <li>
-        <NavigationMenuLink asChild>
-          <a
-              ref={ref}
-              className={cn(
-                  "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                  className
-              )}
-              {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-              {children}
-            </p>
-          </a>
-        </NavigationMenuLink>
+         <NavigationMenuLink asChild>
+            <div className="flex ">
+               <a
+                  ref={ref}
+                  className={cn(
+                     "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                     className,
+                  )}
+                  {...props}
+               >
+                  <div className="flex flex-row gap-2">
+                     <div>{icon}</div>
+                     <div>
+                        <div className="text-sm font-medium leading-none">{title}</div>
+                        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                           {children}
+                        </p>
+                     </div>
+                  </div>
+               </a>
+            </div>
+         </NavigationMenuLink>
       </li>
-  )
-})
-ListItem.displayName = "ListItem"
+   );
+});
+ListItem.displayName = "ListItem";
